2d01ea0bfab6d862b2c87f3a3862553e2e0d6fbb
lulu0206 <luluksaja20@gmail.com>
2022-10-13 03:33:02 +0700

    first commit

---
+++ sesipraktikum2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sesi Praktikum 2</title>
+</head>
+<body>
+    <h2>Materi Pada Sesi Praktikum 2</h2>
+    <P>Sesi praktikum 2
+Membahas tentang apasaja yang bisa di lakukan pada github 
+1. Membuat repositori : merupakan gudang untuk menyimpan project yang telah kita buat
+2. Mengkloning repositori : merupakan cara untuk mengaitkan vs code dengan project yang telah kita push ke repositori sebelumnya
+3. Meremot repositori : merupakan cara kendali repositori lokal dan repositori yang ada di github agar saling berkomunikasi untuk mendorong dan menarik data (pull dan push) 
+4. Mengepush project dari repositori local ke repositori yang ada di git : menambahkan perubahan pada project untuk di upload ke git sehingga git memiliki project versi terbaru
+5. Membuat brach : branch adalah percabangan pengembangan aplikasi untuk membuat versi yang berbeda agar dalam pengembangan berkelompok tidak saling membingungkan antar anggota kelompok </P>
+</body>
+</html>
